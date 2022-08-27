c910d9cc444211c41f549e961cf1806a9b7e769f
yeonhyeeun <0622yeon@naver.com>
2022-08-27 16:49:31 +0900

    make house which have tomato seiling

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ canvas.width = 800;
 canvas.height= 800; 
 
 // x,y,width,heighy : 4argmument of fillRect func 
-ctx.moveTo(50,50); 
-ctx.lineTo(150,50); 
-ctx.lineTo(150,150); 
-ctx.lineTo(50,150); 
-ctx.lineTo(50,50); 
-
+ctx.fillRect(200,200,50,200); 
+ctx.fillRect(400,200,50,200); 
+//ctx.lineWidth = 2; 
+//ctx.strokeRect(300,300,50,100); 
+ctx.fillRect(300,300,50,100); 
+//지붕 덮개 
+ctx.fillRect(200,200,250,20); 
+//지붕 삼각형 
+ctx.fillStyle = "tomato";
+ctx.moveTo(200,200); 
+ctx.lineTo(325,100);
+ctx.lineTo(450,200);
 ctx.fill();
